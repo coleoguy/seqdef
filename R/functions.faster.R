1437b3537bd601ff5fbd594a5a7f080b617e9e63
Heath Blackmon <coleoguy@gmail.com>
2022-06-07 11:51:59 -0500

    Create functions.faster.R

---
+++ R/functions.faster.R
@@ -0,0 +1,81 @@
+# Making a function that will generate syn.imp.vals from a tree and a table. 
+
+SeqDef <- function(tree, df, data.col, invert, scale){
+
+    # The above line stops the code if the tree and data differ in length 
+  if(length(tree$tip.label) != nrow(df)){
+    stop("Tree and Data should have same Length")
+  } 
+  
+  # Tree depth (td) is calculated above 
+  td <- max(branching.times(tree))
+
+    ## TODO
+  # use cophenetic.phylo from APE to make a matrix of distances
+  # then divide the matrix by two to get the distance to the MRCA
+  # the divide the values by the tree depth that you calculate above
+  # then do 1 - the values in the matrix
+  
+  # at this point you should have a matrix that you can look up 
+  # the values that you need below at the second TODO
+  
+  for(i in 1:length(tree$tip.label)){
+    focal.tip <- tree$tip.label[i] #here we look through our tip labels 
+                                    #and make one called "focal tip"
+    x <- c()  # x is an empty vector for use later
+    for(j in 1:length(tree$tip.label)){
+      cur.tip <- tree$tip.label[j]
+      # The first seven lines below will go away instead
+      # of calculating everytime we will just look up the
+      # values that we need for any particular pairing
+      # x <- c(x, foo[row.names(foo)==cur.tip, colnames(foo)==focal.tip] * df[,data.col][.........])
+      if(focal.tip != cur.tip) {
+        x <- c(x, 
+               (1 - ((fastDist(tree, focal.tip, cur.tip)) / 2) / td)* 
+                 df[,data.col][df$species == cur.tip]) 
+      }else{
+        x <- c(x, df[,data.col][df$species == cur.tip])
+      } # lines 26:27 are used in the event that the our focal tip and cur.tip
+        # are in fact the same. when this occurs we just get back the imp.val. 
+    }
+    if(invert){
+      df$syn.imp.vals[df$species == focal.tip] <- 1 - mean(x) 
+    }else{
+      df$syn.imp.vals[df$species == focal.tip] <- mean(x) 
+    }
+# line 32 stores the mean value of x that we got from earlier inside a 
+# data.frame as "synthetic importance values" (syn.imp.vals) 
+    # when the species name is 
+# the same as the focal.tip we were comparing our cur.tip with. 
+  }
+    if(scale){
+      results <- (df$syn.imp.vals - min(df$syn.imp.vals)) /
+      (max(df$syn.imp.vals) -min(df$syn.imp.vals))
+    }else{
+      results <- df$syn.imp.vals
+    }
+  return(results)
+}
+
+# Lines 38:39 are used to normalize syn.imp.vals into values between 0 and 1.
+# Line 40 returns our data frame with our newly calculated synthetic importance 
+#values. 
+
+# Incorporating the Data data
+#given a tree with random amounts of sequence data (table), 
+#and a tree (0 being no data, 
+# and 1 being all the data)... we need to find a way to calculate which  
+# species need to be sequenced more... and then the ----- of this value, 
+# and our synthetic importance value will give overall sequence desirability. 
+
+
+PlotSeqDef <- function(tree, df, data.col){
+  tree$edge.length <- tree$edge.length/ max(branching.times(tree))
+  plot(tree, cex=.7)
+  tiplabels(text=df[, data.col], adj=-1.5, frame="none", col="red",cex=.7)
+  tiplabels(text=as.character(df[, 4]), offset=-.1, cex=.7,frame="none", col="blue")
+}
+
+
+
+
