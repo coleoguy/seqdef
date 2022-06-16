2ab26cae5391c6937a5b862271665ca937f47c34
GarrettF1 <106110704+GarrettF1@users.noreply.github.com>
2022-06-16 09:57:26 -0500

    Tested Function

---
+++ Testing/Testing 2.0.R
@@ -0,0 +1,20 @@
+
+# Loading in packages
+library(ape)
+library(phytools)
+# Creating a phylo object
+tree <- pbtree(n= 10, scale=1)
+plot(tree)
+
+# Generaring sample data for both 'importance' and 'data'
+imp.vals <- sample( c(0,1), size= length(tree$tip.label),
+                    replace= T,prob=c(.8,.2))
+
+data.vals <- sample( c(0,1), size= length(tree$tip.label),
+                     replace= T,prob=c(.8,.2))
+# Combining the synthesized values into a single data frame. 
+df <- data.frame(imp.vals, data.vals)
+
+SeqDef(tree, df, data.col = 1, invert = T, scale = T )
+                    
+
